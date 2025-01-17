144527fcfd4c3c6475f71f844cdd7120624c0062
Niclas Doll <niclas.doll@icloud.com>
2025-01-17 14:14:54 +0100

    Test

--- tests/hyped/extensions/nlp/nodes/utils/test_jinja2.py
+++ tests/hyped/extensions/nlp/nodes/utils/test_jinja2.py
@@ -9,3 +9,11 @@ class TestJinja2Processor(BaseDataProcessorTest):
     input_data = [{"value": 0}, {"value": 1}, {"value": 2}, {"value": 3}]
     expected_output_feature = String
     expected_output_data = ["Value: 0", "Value: 1", "Value: 2", "Value: 3"]
+
+
+class TestJinja2ProcessorWithConditionalLogic(BaseDataProcessorTest):
+    processor = Jinja2Processor(template="{% if value > 2 %}Large{% else %}Small{% endif %}")
+    input_features = {"value": Int}
+    input_data = [{"value": 0}, {"value": 1}, {"value": 2}, {"value": 3}]
+    expected_output_feature = String
+    expected_output_data = ["Small", "Small", "Small", "Large"]
